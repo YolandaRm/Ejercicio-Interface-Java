17dedd44032709ab4d4b0bcc6d01bdb81dcf9b46
YolandaRM <yramosmartin@gmail.com>
2020-02-11 13:10:59 +0100

    first commit

---
+++ netflix/src/test/java/netflix/AppTest.java
@@ -0,0 +1,20 @@
+package netflix;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
