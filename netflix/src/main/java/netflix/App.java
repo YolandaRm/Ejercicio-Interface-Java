cb176d8d96a50fe0cf27acfbbd6c654ef1a50cf5
YolandaRM <yramosmartin@gmail.com>
2020-02-12 12:43:38 +0100

    a

--- netflix/src/main/java/netflix/App.java
+++ netflix/src/main/java/netflix/App.java
@@ -11,3 +11,50 @@ public class App
         System.out.println( "Hello World!" );
     }
 }
+
+interface IReproducible{
+    public void iniciar();
+    public void pausar();
+    public void Adelantar();
+    public void Retroceder();
+}
+
+abstract class Contenido{
+    public String Titulo;
+    public String Descripcion;
+    public String Fecha;
+    public String Director;
+    public String Productora;
+
+    Contenido (Titulo, Descripcion, Fecha, Director, Productora){
+        this.Titulo = Titulo;
+        this.Descripcion = Descripcion;
+        this.Fecha = Fecha;
+        this.Director = Director;
+        this.Productora = Productora;
+    }
+}
+
+
+public class Pelicula implements IReproducible{
+   
+    }
+
+}
+
+class Series{
+
+}
+
+class Capitulos implements IReproducible{
+
+}
+
+
+
+
+
+
+
+
+
